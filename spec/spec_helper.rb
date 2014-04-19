8dd43a672578b2d675dc7e5cc547fffd91b40b36
Brad Chase <bosco87@gmail.com>
2014-04-19 16:04:35 -0400

    start rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require File.expand_path('../../lib/haip.rb', __FILE__)
+
+require 'rubygems'
+require 'rspec'
+
+Dir["spec/support/**/*.rb"].each { |f| require File.expand_path(f) }
+
+RSpec.configure do |config|
+end
