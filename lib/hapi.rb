0ef9a138a9e020aaa770c2b50b9afcb9df76eb1e
Brad Chase <bosco87@gmail.com>
2014-04-19 16:56:30 -0400

    requires; remove comment

--- lib/hapi.rb
+++ lib/hapi.rb
@@ -1,4 +1,10 @@
+require 'active_support/core_ext/module/attribute_accessors'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/json'
+require 'restclient'
+
 require 'hapi/version'
+
 require 'hapi/service'
 
 module Hapi
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Hapi::Service do
-  # Hapi::Service.new 
-  #   accepts a string url for the service
-  #   returns a Class that knows about url and provides classes
-
   let(:url) { 'http://api.deckbrew.com/mtg' }
 
   let(:service) { Hapi::Service.new url }
